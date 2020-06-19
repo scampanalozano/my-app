83a6fe8a191ba75ce2bec74713fb658c7be04ab2
scampanalozano <antonellacampana6@gmail.com>
2020-06-19 18:32:43 -0500

    inicio commit

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header(){
+    return (
+        <div>// esta es la cabecera
+        <div> <img src=""/></div>
+        <div>
+          <div>
+            <button>login</button>
+          </div>
+
+        </div>
+      </div>
+
+    );
+}
+export default Header;// para que este disponible para otros archivos 
