ee385dfa750d42a4a02649cdf703bd9a2bf11f5a
Pablo Correa <pcorrea@thoughtworks.com>
2020-06-19 20:02:10 -0500

    adds react bootstrap dependency

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+import Alert from 'react-bootstrap/Alert'
 
-function Header(){
-    return (
-        <div>// esta es la cabecera
-        <div> <img src=""/></div>
-        <div>
-          <div>
-            <button>login</button>
-          </div>
-
-        </div>
-      </div>
-
-    );
+function Header() {
+  return (
+    <Container>
+      <Row className="Header-banner">
+        <Col>1 of 2</Col>
+        <Col>2 of 2</Col>
+      </Row>
+      <Row>
+        <Col>1 of 3</Col>
+        <Col>2 of 3</Col>
+        <Col>3 of 3</Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button variant="primary" type="submit">click</Button>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 export default Header;// para que este disponible para otros archivos 
