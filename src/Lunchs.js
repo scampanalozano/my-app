d34d9f652f79c4c1064f9c017a97d315ea306a02
scampanalozano <antonellacampana6@gmail.com>
2020-07-18 23:36:49 -0500

    creacion de la funcionalidad lunchs

--- src/Lunchs.js
+++ src/Lunchs.js
@@ -1,10 +1,87 @@
-import React from 'react';
+
+import React, { useState } from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from "react-bootstrap/Form";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import Image from "react-bootstrap/Image"
+import osolunch from './osolunch.gif';
+import Button from "react-bootstrap/Button"
+import "./Lunchs.css";
 
 
-const Lunchs =()=>{
-return <div>
-    </div>
+
+
+
+
+const Lunchs = () => {
+    const [fechaDeNacimiento, setFechaDeNacimiento] = useState(new Date())
+    const [lunch, setLunch] = useState({});
+
+
+
+    const generarLunch = (e) => {
+        console.log(`${fechaDeNacimiento}`);
+        const lunch = generarLunch(fechaDeNacimiento)
+        setLunch(lunch);
+    };
+
+
+
+
+    return (
+        <Row>
+            <Col>
+                <Form>
+                    <Row><Col className="titulo">Seleccione la fecha de nacimiento</Col>
+                    </Row>
+                    <Row>
+                        <Col lg="1" md="1" />
+                        <Col lg="3" md="10" />
+                        <Row>
+                            <Col lg="4" md="4" className="fecha">Fecha de nacimiento</Col>
+                            <Col>
+                                <DatePicker selected={fechaDeNacimiento}
+                                    onChange={setFechaDeNacimiento}
+
+                                />
+
+                            </Col>
+
+                        </Row>
+
+                    </Row>
+                </Form>
+
+                <Row>
+                    <Col>
+                        <Image className="oso"
+                            src={osolunch}
+                            fluid
+                        />
+
+                    </Col>
+                </Row>
+
+                <Row>
+
+                    <Col>
+                        <Button variant="info" className="boton" onClick={generarLunch}>Generar</Button>{' '}
+                    </Col>
+                </Row>
+
+
+            </Col>
+
+
+
+        </Row>
+
+
+    )
 
 }
+
 
 export default Lunchs;
